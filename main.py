952b03a119df55c8571c5da782b783551ea521f5
mlmerrr <chadin.bunn@gmail.com>
2025-04-18 21:20:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, BU 1670708286'
+    return 'Hello, BU 2'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    app.run(debug=True)
